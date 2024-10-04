e5473685bab89d43e94ba0a46e92b81e8a6fc02c
Jahmil <jgivner@enrise.com>
2024-10-04 17:34:00 +0200

    added .git to gitignore

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ echo "PokeScouter";
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pokedex</title>
+    <title>PokeScouter</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body class="body">
